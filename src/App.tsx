1aff9885778b865bb5149603d86d9f8f930d5899
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2023-01-04 17:07:47 +0300

    app finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Flex, Group, Text, Textarea, Title } from "@mantine/core";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [textInput, setTextInput] = useState<string>("");
+  const [wordCount, setWordCount] = useState(0);
+  const [readingTime, setReadingTime] = useState(0);
+
+  useEffect(() => {
+    textInput.length > 0 && countWords();
+  }, [textInput]);
+
+  useEffect(() => {
+    calculate();
+  }, [wordCount]);
+
+  const handleTextChange = (e: any) => {
+    console.log(e);
+    setTextInput(e.target.value);
+  };
+
+  const countWords = () => {
+    const arr = textInput.split(" ");
+    setWordCount(arr.filter((word) => word !== "").length);
+  };
+
+  const calculate = () => {
+    setReadingTime((wordCount / 150) * 60);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex direction="column" gap="lg" mt="20vh">
+      <Title>Metin Süre Hesaplayıcı</Title>
+      <Text>
+        Lütfen Metni Giriniz
+        <Text size="xs">
+          Okuma hızı dakikada 150 kelime olarak belirlenmiştir.
+        </Text>
+      </Text>
+
+      <Textarea
+        placeholder="Lütfen Okuma Süresi Hesaplanacak Metni Giriniz."
+        minRows={10}
+        label="Metin:"
+        radius="md"
+        size="md"
+        withAsterisk
+        value={textInput}
+        onChange={handleTextChange}
+      />
+
+      {readingTime > 0 && (
+        <Group align="center" mt="lg" position="center">
+          <Text size={20}>Kelime Sayısı: {wordCount} </Text>
+          <Text size={20}>Okuma Süresi: {readingTime}sn </Text>
+        </Group>
+      )}
+    </Flex>
   );
 }
 
